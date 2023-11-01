a07c80366daf12cd26dafd23e646889bf0bbd98d
Oswaldo Jales <oswaldojales@gmail.com>
2023-11-01 00:54:05 -0300

    update20231101 0053

---
+++ consulta-psi/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <p className="texto"> Consulta Psi</p>
+        <a className="texto" href="#">Entrar</a>
+      </header>
+
+      <section id="center">
+        <nav>
+          <ul id="navegador">
+            <li><a href="#">Início</a></li>
+            <li><a href="#">Agenda</a></li>
+            <li><a href="#">Pacientes</a></li>
+            <li><a href="#">Consultas</a></li>
+            <li><a href="#">Transcrições</a></li>
+          </ul>
+        </nav>
+
+      </section>
+
+     
+
+
+    </div>
+  );
+}
+
+export default App;
